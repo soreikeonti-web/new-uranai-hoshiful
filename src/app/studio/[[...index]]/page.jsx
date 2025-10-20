d1ebdee12707b5f276803b534d42f14c38233063
soreikeonti-web <soreikeonti@gmail.com>
2025-10-20 21:24:03 +0900

    "fix: Vercelビルド修正のため next-sanity/studio を sanity Studio に置き換え"

--- src/app/studio/[[...index]]/page.jsx
+++ src/app/studio/[[...index]]/page.jsx
@@ -1,19 +1,12 @@
-/**
- * This route is responsible for the built-in authoring environment using Sanity Studio.
- * All routes under your studio path is handled by this file using Next.js' catch-all routes:
- * https://nextjs.org/docs/routing/dynamic-routes#catch-all-routes
- *
- * You can learn more about the next-sanity package here:
- * https://github.com/sanity-io/next-sanity
- */
+'use client'
 
-import { NextStudio } from 'next-sanity/studio'
+// next-sanity/studio は使わず、公式の Studio コンポーネントを直に使う
+import { Studio } from 'sanity'
 import config from '../../../../sanity.config'
 
+// Studio は静的でもOK。必要なら 'force-dynamic' にしても可
 export const dynamic = 'force-static'
 
-export { metadata, viewport } from 'next-sanity/studio'
-
 export default function StudioPage() {
-  return <NextStudio config={config} />
+  return <Studio config={config} />
 }
