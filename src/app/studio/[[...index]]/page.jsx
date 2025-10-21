1b781204339129feb2a90c5672e29da8093f9fc6
soreikeonti-web <soreikeonti@gmail.com>
2025-10-21 19:30:08 +0900

    パスを修正../../../../

--- src/app/studio/[[...index]]/page.jsx
+++ src/app/studio/[[...index]]/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Studio } from 'sanity'
-import config from '../../../sanity.config'  // ← 修正ポイント①
+import config from '../../../../sanity.config'  // ← 修正ポイント①
 
 // Studio は動的な方が安全（本番でエラー回避できる）
 export const dynamic = 'force-dynamic'       // ← 修正ポイント②
