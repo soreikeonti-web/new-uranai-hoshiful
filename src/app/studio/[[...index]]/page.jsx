23cadf1440e7055e31a99112d5cbb0a992c6aa5f
soreikeonti-web <soreikeonti@gmail.com>
2025-10-21 19:21:42 +0900

    fix: 修正 - Sanity Studio ページで本番エラーが出る問題を修正

--- src/app/studio/[[...index]]/page.jsx
+++ src/app/studio/[[...index]]/page.jsx
@@ -1,11 +1,10 @@
 'use client'
 
-// next-sanity/studio は使わず、公式の Studio コンポーネントを直に使う
 import { Studio } from 'sanity'
-import config from '../../../../sanity.config'
+import config from '../../../sanity.config'  // ← 修正ポイント①
 
-// Studio は静的でもOK。必要なら 'force-dynamic' にしても可
-export const dynamic = 'force-static'
+// Studio は動的な方が安全（本番でエラー回避できる）
+export const dynamic = 'force-dynamic'       // ← 修正ポイント②
 
 export default function StudioPage() {
   return <Studio config={config} />
